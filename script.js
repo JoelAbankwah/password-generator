c4baa405743e837572589fc19e77732078bceaa6
JoelAbankwah <100015338+JoelAbankwah@users.noreply.github.com>
2022-03-22 17:37:05 -0700

    cleaning up code / adding comments

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
-// Assignment code here
-/* GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-*/
-
 // Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
-var lower = ['abcdefghijklmnopqrstuvwxyz' ]
-var upper = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ' ]
-var numeric = "1234567890"
-var specialCharacter = "!@#$%^&*()-+/"
+var lowerCharacter = ['abcdefghijklmnopqrstuvwxyz' ]
+var upperCharacters = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ' ]
+var numericCharacters = "1234567890"
+var specialCharacters = "!@#$%^&*()-+/"
 var potentialCharacters = ""
 var passwordText = ""
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -49,40 +32,44 @@ function writePassword() {
         function confirmTypes() {
             password.mixed = window.confirm("Would you like to include upper and lowercase characters?")
             if (password.mixed === false) {
-               var lowerCase = window.confirm("Would you like to include lowercase characters?")
+               var lowerCase = window.confirm("Would you like to include only lowercase characters?")
                if (lowerCase === false) {
-                   var upperCase = window.confirm("Would you like to include uppercase characters?")
+                   var upperCase = window.confirm("Would you like to include only uppercase characters?")
                } 
             }
             password.num = window.confirm("Would you like to include numeric characters?")
-
+            
             password.special = window.confirm("Would you like to include special characters?")
 
+            //checks to see that at least 1 of the character types have been selected, if not it reloops to the top of the function
             if (password.mixed === false && password.num === false && password.special === false && lowerCase === false && upperCase === false) {
                 window.alert("You need to select at least 1 character type.")
                 confirmTypes();
             } 
+            //checks to see if each character type is true, and if it is true it adds that string into the string of possible characters to use for the random generator
             if (password.mixed) {
-                potentialCharacters = potentialCharacters.concat(upper, lower)
+                potentialCharacters = potentialCharacters.concat(upperCharacters, lowerCharacter)
             }
             if (lowerCase) {
-                potentialCharacters = potentialCharacters.concat(lower)    
+                potentialCharacters = potentialCharacters.concat(lowerCharacter)    
             }
             if (upperCase) {
-                potentialCharacters = potentialCharacters.concat(upper)
+                potentialCharacters = potentialCharacters.concat(upperCharacters)
             }
             if (password.num) {
-                potentialCharacters = potentialCharacters.concat(numeric)
+                potentialCharacters = potentialCharacters.concat(numericCharacters)
             }
             if (password.special) {
-                potentialCharacters = potentialCharacters.concat(specialCharacter)
+                potentialCharacters = potentialCharacters.concat(specialCharacters)
             }
         }
 
+        //With each loop password text is equal to password text plus a random character between the two indices (randonum & randnoum +1)
         for (var i = 0; i <= legnth; i++) {
             var randomNumber = Math.floor(Math.random() * potentialCharacters.length);
             passwordText += potentialCharacters.substring(randomNumber, randomNumber +1);
         }
+        //sets the password id to the value of the passwordText
         document.querySelector("#password").value = passwordText;
     }
     generatePassword(); 
